3feeb2e13696e95233906285824484d5aee68c22
asharansari <ashar.ansari@1silverbullet.tech>
2021-04-21 11:27:45 +0530

    Updates

---
+++ precache-manifest.7234af94e7793ee347f935504a75e97f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717b9d45ca7aaba8c01481f8435557bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b564ee3816825038fcf",
+    "url": "/static/css/2.963765e0.chunk.css"
+  },
+  {
+    "revision": "080c6275709cfc40729c",
+    "url": "/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "0b564ee3816825038fcf",
+    "url": "/static/js/2.99e2713c.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/static/js/2.99e2713c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080c6275709cfc40729c",
+    "url": "/static/js/main.ca5c3e2d.chunk.js"
+  },
+  {
+    "revision": "2613c536928d3c0c7406",
+    "url": "/static/js/runtime-main.1384b09c.js"
+  },
+  {
+    "revision": "adca41d3777e83416e7ad919fa0da8ac",
+    "url": "/static/media/dbank.adca41d3.png"
+  }
+]);
